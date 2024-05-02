7e884dc82d2c5a2106de54231e589640b1b50e6c
Wilmar Andres Rojas Villafañe <106172929+wirovi07@users.noreply.github.com>
2024-05-02 17:14:01 -0500

    Register

--- src/views/pages/Register.vue
+++ src/views/pages/Register.vue
@@ -7,37 +7,58 @@
             <CCardBody class="p-4">
               <CForm>
                 <h1>Register</h1>
-                <p class="text-body-secondary">Create your account</p>
-                <CInputGroup class="mb-3">
-                  <CInputGroupText>
-                    <CIcon icon="cil-user" />
-                  </CInputGroupText>
-                  <CFormInput placeholder="Username" autocomplete="username" />
-                </CInputGroup>
-                <CInputGroup class="mb-3">
-                  <CInputGroupText>@</CInputGroupText>
-                  <CFormInput placeholder="Email" autocomplete="email" />
-                </CInputGroup>
-                <CInputGroup class="mb-3">
-                  <CInputGroupText>
-                    <CIcon icon="cil-lock-locked" />
-                  </CInputGroupText>
-                  <CFormInput
-                    type="password"
-                    placeholder="Password"
-                    autocomplete="new-password"
-                  />
-                </CInputGroup>
-                <CInputGroup class="mb-4">
-                  <CInputGroupText>
-                    <CIcon icon="cil-lock-locked" />
-                  </CInputGroupText>
-                  <CFormInput
-                    type="password"
-                    placeholder="Repeat password"
-                    autocomplete="new-password"
-                  />
-                </CInputGroup>
+                <a class="text-primary text-body-secondary text-decoration-none fs-6 fw-bold">Crea tu cuenta!</a>
+                <div class="row mt-4">
+                  <div class="col-md-6">
+                    <CInputGroup class="mb-3">
+                      <CInputGroupText>
+                        <CIcon icon="cil-user" />
+                      </CInputGroupText>
+                      <CFormInput placeholder="Username" autocomplete="username" />
+                    </CInputGroup>
+                    <CInputGroup class="mb-3">
+                      <CInputGroupText>@</CInputGroupText>
+                      <CFormInput placeholder="Email" autocomplete="email" />
+                    </CInputGroup>
+                    <CInputGroup class="mb-3">
+                      <CInputGroupText>
+                        <CIcon icon="cil-lock-locked" />
+                      </CInputGroupText>
+                      <CFormInput type="password" placeholder="Password" autocomplete="new-password" />
+                    </CInputGroup>
+                    <CInputGroup class="mb-4">
+                      <CInputGroupText>
+                        <CIcon icon="cil-lock-locked" />
+                      </CInputGroupText>
+                      <CFormInput type="password" placeholder="Repeat password" autocomplete="new-password" />
+                    </CInputGroup>
+                  </div>
+                  <div class="col-md-6">
+                    <CInputGroup class="mb-3">
+                      <CInputGroupText>
+                        <CIcon icon="cil-user" />
+                      </CInputGroupText>
+                      <CFormInput placeholder="Username" autocomplete="username" />
+                    </CInputGroup>
+                    <CInputGroup class="mb-3">
+                      <CInputGroupText>@</CInputGroupText>
+                      <CFormInput placeholder="Email" autocomplete="email" />
+                    </CInputGroup>
+                    <CInputGroup class="mb-3">
+                      <CInputGroupText>
+                        <CIcon icon="cil-lock-locked" />
+                      </CInputGroupText>
+                      <CFormInput type="password" placeholder="Password" autocomplete="new-password" />
+                    </CInputGroup>
+                    <CInputGroup class="mb-4">
+                      <CInputGroupText>
+                        <CIcon icon="cil-lock-locked" />
+                      </CInputGroupText>
+                      <CFormInput type="password" placeholder="Repeat password" autocomplete="new-password" />
+                    </CInputGroup>
+                  </div>
+                </div>
+
                 <div class="d-grid">
                   <CButton color="success">Create Account</CButton>
                 </div>
