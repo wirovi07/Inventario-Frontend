c6f9d7d16bb21b2da67cbe80469af8c8743daed3
Wilmar Andres Rojas Villafañe <106172929+wirovi07@users.noreply.github.com>
2024-05-07 12:55:26 -0500

    Vista Register

--- src/views/pages/Register.vue
+++ src/views/pages/Register.vue
@@ -13,37 +13,35 @@
                 </div>
                 <div class="row mt-4">
                   <div class="col-md-6">
-                    <CInputGroup class="mb-3">
-                      <div>
-                        <CInputGroupText>
-                          <CIcon icon="cil-description" />
-                        </CInputGroupText>
-                        <select v-model="formData.type_document" class="form-control">
-                          <option value="" disabled selected>Tipo de Documento</option>
-                          <option value="RC">RC</option>
-                          <option value="TI">TI</option>
-                          <option value="CC">CC</option>
-                          <option value="EX">EX</option>
-                        </select>
-                      </div>
-                      <template v-if="errors.type_document.length > 0">
-                        <b :key="e" v-for="e in errors.type_document" class="text-danger">
-                            {{ e }}
-                        </b>
-                    </template>
+                    <CInputGroup class="mt-3">
+                      <CInputGroupText>
+                        <CIcon icon="cil-description" />
+                      </CInputGroupText>
+                      <select v-model="formData.type_document" class="form-control">
+                        <option value="" disabled selected>Tipo de Documento</option>
+                        <option value="RC">RC</option>
+                        <option value="TI">TI</option>
+                        <option value="CC">CC</option>
+                        <option value="EX">EX</option>
+                      </select>
                     </CInputGroup>
-                    <CInputGroup class="mb-3">
+                    <template v-if="errors.type_document.length > 0">
+                      <b :key="e" v-for="e in errors.type_document" class="text-danger">
+                        {{ e }}
+                      </b>
+                    </template>
+                    <CInputGroup class="mt-3">
                       <CInputGroupText>
                         <CIcon icon="cil-contact" />
                       </CInputGroupText>
                       <CFormInput v-model="formData.first_name" placeholder="Nombres" type="text" tabindex="3" />
-                      <template v-if="errors.first_name.length > 0">
-                        <b :key="e" v-for="e in errors.first_name" class="text-danger">
-                            {{ e }}
-                        </b>
-                    </template>
                     </CInputGroup>
-                    <CInputGroup class="mb-3">
+                    <template v-if="errors.first_name.length > 0">
+                      <b :key="e" v-for="e in errors.first_name" class="text-danger">
+                        {{ e }}
+                      </b>
+                    </template>
+                    <CInputGroup class="mt-3">
                       <CInputGroupText>
                         <CIcon icon="cil-wc" />
                       </CInputGroupText>
@@ -54,54 +52,95 @@
                         <option value="2">Femenino</option>
                       </select>
                     </CInputGroup>
-                    <CInputGroup class="mb-4">
+                    <template v-if="errors.sex.length > 0">
+                      <b :key="e" v-for="e in errors.sex" class="text-danger">
+                        {{ e }}
+                      </b>
+                    </template>
+                    <CInputGroup class="mt-3">
                       <CInputGroupText>
                         <CIcon icon="cil-phone" />
                       </CInputGroupText>
-                      <CFormInput v-model="formData.phone" placeholder="Telefono" type="text" tabindex="7" />
+                      <CFormInput v-model="formData.phone" placeholder="Telefono" type="numeric" tabindex="7" />
                     </CInputGroup>
-                    <CInputGroup class="mb-4">
+                    <template v-if="errors.phone.length > 0">
+                      <b :key="e" v-for="e in errors.phone" class="text-danger">
+                        {{ e }}
+                      </b>
+                    </template>
+                    <CInputGroup class="mt-3">
                       <CInputGroupText>
                         <CIcon icon="cil-lock-locked" />
                       </CInputGroupText>
                       <CFormInput v-model="formData.password" type="password" placeholder="Contraseña" tabindex="9" />
                     </CInputGroup>
+                    <template v-if="errors.password.length > 0">
+                      <b :key="e" v-for="e in errors.password" class="text-danger">
+                        {{ e }}
+                      </b>
+                    </template>
                   </div>
                   <div class="col-md-6">
-                    <CInputGroup class="mb-3">
+                    <CInputGroup class="mt-3">
                       <CInputGroupText>
                         <CIcon icon="cil-user" />
                       </CInputGroupText>
-                      <CFormInput v-model="formData.document" placeholder="Documento" type="text" tabindex="2" />
+                      <CFormInput v-model="formData.document" placeholder="Documento" type="numeric" tabindex="2" />
                     </CInputGroup>
-                    <CInputGroup class="mb-3">
+                    <template v-if="errors.document.length > 0">
+                      <b :key="e" v-for="e in errors.document" class="text-danger">
+                        {{ e }}
+                      </b>
+                    </template>
+                    <CInputGroup class="mt-3">
                       <CInputGroupText>
                         <CIcon icon="cil-contact" />
                       </CInputGroupText>
                       <CFormInput v-model="formData.last_name" placeholder="Apellido" type="text" tabindex="4" />
                     </CInputGroup>
-                    <CInputGroup class="mb-3">
+                    <template v-if="errors.last_name.length > 0">
+                      <b :key="e" v-for="e in errors.last_name" class="text-danger">
+                        {{ e }}
+                      </b>
+                    </template>
+                    <CInputGroup class="mt-3">
                       <CInputGroupText>
                         <CIcon icon="cil-address-book" />
                       </CInputGroupText>
                       <CFormInput v-model="formData.address" placeholder="Dirección" type="text" tabindex="6" />
                     </CInputGroup>
-                    <CInputGroup class="mb-4">
+                    <template v-if="errors.address.length > 0">
+                      <b :key="e" v-for="e in errors.address" class="text-danger">
+                        {{ e }}
+                      </b>
+                    </template>
+                    <CInputGroup class="mt-3">
                       <CInputGroupText>
                         <CIcon icon="cil-at" />
                       </CInputGroupText>
                       <CFormInput v-model="formData.email" placeholder="Email" type="email" tabindex="8" />
                     </CInputGroup>
-                    <CInputGroup class="mb-4">
+                    <template v-if="errors.email.length > 0">
+                      <b :key="e" v-for="e in errors.email" class="text-danger">
+                        {{ e }}
+                      </b>
+                    </template>
+                    <CInputGroup class="mt-3">
                       <CInputGroupText>
                         <CIcon icon="cil-lock-unlocked" />
                       </CInputGroupText>
-                      <CFormInput v-model="formData.password_confirmed" type="password" placeholder="Confirmar contraseña" tabindex="10" />
+                      <CFormInput v-model="formData.password_confirmed" type="password"
+                        placeholder="Confirmar contraseña" tabindex="10" />
                     </CInputGroup>
+                    <template v-if="errors.password_confirmed.length > 0">
+                      <b :key="e" v-for="e in errors.password_confirmed" class="text-danger">
+                        {{ e }}
+                      </b>
+                    </template>
                   </div>
                 </div>
-                <div class="d-grid">
-                  <CButton  @click="registerUser" color="success">Registrate!</CButton>
+                <div class="d-grid mt-3">
+                  <CButton @click="registerUser" color="success">Registrate!</CButton>
                 </div>
               </CForm>
             </CCardBody>
@@ -166,47 +205,57 @@ export default {
       }
     }
 
-    
-  const registerUser = async () => {
+    const registerUser = async () => {
       errorsClear()
 
       let has_error = false;
       Object.entries(formData.value).forEach(f => {
-          const elemento = f[0]
-          const value = f[1]
-          if (value == "") {
-              has_error = true
-              errors.value[elemento] = "Este campo es obligatorio"
-          }
+        const elemento = f[0]
+        const value = f[1]
+        if (value == "") {
+          has_error = true
+          errors.value[elemento] = "Este campo es obligatorio"
+        }
       });
 
       if (has_error) return;
 
       try {
-          const response = await axios.post('http://inventario-backend.test/api/register', formData.value);
+        const response = await axios.post('http://inventario-backend.test/api/register', formData.value);
 
-          Swal.fire({
-              title: 'Éxito!',
-              text: 'Usuario registrado correctamente!',
-              icon: 'success',
-              confirmButtonText: '¡Entendido!'
-          });
+        Swal.fire({
+          title: 'Éxito!',
+          text: 'Usuario registrado correctamente!',
+          icon: 'success',
+          confirmButtonText: '¡Entendido!'
+        });
 
-          router.push({ name: 'login' })
+        formData.value = {
+            type_document: '',
+            document: '',
+            first_name: '',
+            last_name: '',
+            sex: '',
+            address: '',
+            phone: '',
+            email: '',
+            password: '',
+            password_confirmed: '',
+        };
+
+        router.push({ name: 'login' })
 
       } catch (error) {
-          if (error.response && error.response.data && error.response.data.errors) {
-              const errors_api = error.response.data.errors;
-              Object.entries(errors_api).forEach(e => {
-                  const elemento = e[0]
-                  const mensaje = e[1]
-                  errors.value[elemento] = mensaje
-              });
-          } else {
-              alert("server error")
-          }
+        if (error.response && error.response.data && error.response.data.errors) {
+          const errors_api = error.response.data.errors;
+          Object.entries(errors_api).forEach(e => {
+            const elemento = e[0]
+            const mensaje = e[1]
+            errors.value[elemento] = mensaje
+          });
+        } 
       }
-  };
+    };
 
     return {
       formData,
