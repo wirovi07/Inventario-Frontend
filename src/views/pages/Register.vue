19f7e4f206ff432867da47d3ea175051fcecc857
Wilmar Andres Rojas Villafañe <106172929+wirovi07@users.noreply.github.com>
2024-05-06 22:35:23 -0500

    Modificaciones Register

--- src/views/pages/Register.vue
+++ src/views/pages/Register.vue
@@ -8,39 +8,63 @@
               <CForm>
                 <h1 class="text-center fw-semibold">Registrate ó Crea tu cuenta</h1>
                 <div class="text-center">
-                    <span class="text-primary text-body-secondary fs-6 fw-medium hover-effect">Ya tienes una cuenta? <a href="" class="primary fst-normal text-decoration-none">Ingresar</a></span>
+                  <span class="text-primary text-body-secondary fs-6 fw-medium hover-effect">Ya tienes una cuenta? <a
+                      href="" class="primary fst-normal text-decoration-none">Ingresar</a></span>
                 </div>
                 <div class="row mt-4">
                   <div class="col-md-6">
                     <CInputGroup class="mb-3">
-                      <CInputGroupText>
-                        <CIcon icon="cil-description" />
-                      </CInputGroupText>
-                      <CFormInput placeholder="Tipo Documento" type="text" tabindex="1"/>
+                      <div>
+                        <CInputGroupText>
+                          <CIcon icon="cil-description" />
+                        </CInputGroupText>
+                        <select v-model="formData.type_document" class="form-control">
+                          <option value="" disabled selected>Tipo de Documento</option>
+                          <option value="RC">RC</option>
+                          <option value="TI">TI</option>
+                          <option value="CC">CC</option>
+                          <option value="EX">EX</option>
+                        </select>
+                      </div>
+                      <template v-if="errors.type_document.length > 0">
+                        <b :key="e" v-for="e in errors.type_document" class="text-danger">
+                            {{ e }}
+                        </b>
+                    </template>
                     </CInputGroup>
                     <CInputGroup class="mb-3">
                       <CInputGroupText>
-                        <CIcon icon="cil-contact"/>
+                        <CIcon icon="cil-contact" />
                       </CInputGroupText>
-                      <CFormInput placeholder="Nombres" type="text" tabindex="3"/>
+                      <CFormInput v-model="formData.first_name" placeholder="Nombres" type="text" tabindex="3" />
+                      <template v-if="errors.first_name.length > 0">
+                        <b :key="e" v-for="e in errors.first_name" class="text-danger">
+                            {{ e }}
+                        </b>
+                    </template>
                     </CInputGroup>
                     <CInputGroup class="mb-3">
                       <CInputGroupText>
                         <CIcon icon="cil-wc" />
                       </CInputGroupText>
-                      <CFormInput placeholder="Sexo" type="text" tabindex="5"/>
+                      <select v-model="formData.sex" class="form-control">
+                        <option value="" disabled selected>Genero
+                        </option>
+                        <option value="1">Masculino</option>
+                        <option value="2">Femenino</option>
+                      </select>
                     </CInputGroup>
                     <CInputGroup class="mb-4">
                       <CInputGroupText>
-                        <CIcon icon="cil-phone"/>
+                        <CIcon icon="cil-phone" />
                       </CInputGroupText>
-                      <CFormInput placeholder="Telefono" type="text" tabindex="7"/>
+                      <CFormInput v-model="formData.phone" placeholder="Telefono" type="text" tabindex="7" />
                     </CInputGroup>
                     <CInputGroup class="mb-4">
                       <CInputGroupText>
                         <CIcon icon="cil-lock-locked" />
                       </CInputGroupText>
-                      <CFormInput type="password" placeholder="Contraseña" tabindex="9"/>
+                      <CFormInput v-model="formData.password" type="password" placeholder="Contraseña" tabindex="9" />
                     </CInputGroup>
                   </div>
                   <div class="col-md-6">
@@ -48,36 +72,36 @@
                       <CInputGroupText>
                         <CIcon icon="cil-user" />
                       </CInputGroupText>
-                      <CFormInput placeholder="Documento" type="text" tabindex="2"/>
+                      <CFormInput v-model="formData.document" placeholder="Documento" type="text" tabindex="2" />
                     </CInputGroup>
                     <CInputGroup class="mb-3">
                       <CInputGroupText>
-                        <CIcon icon="cil-contact"/>
+                        <CIcon icon="cil-contact" />
                       </CInputGroupText>
-                      <CFormInput placeholder="Apellido" type="text" tabindex="4"/>
+                      <CFormInput v-model="formData.last_name" placeholder="Apellido" type="text" tabindex="4" />
                     </CInputGroup>
                     <CInputGroup class="mb-3">
                       <CInputGroupText>
                         <CIcon icon="cil-address-book" />
                       </CInputGroupText>
-                      <CFormInput placeholder="Dirección" type="text" tabindex="6"/>
+                      <CFormInput v-model="formData.address" placeholder="Dirección" type="text" tabindex="6" />
                     </CInputGroup>
                     <CInputGroup class="mb-4">
                       <CInputGroupText>
-                        <CIcon icon="cil-at"/>
+                        <CIcon icon="cil-at" />
                       </CInputGroupText>
-                      <CFormInput placeholder="Email" type="email" tabindex="8"/>
+                      <CFormInput v-model="formData.email" placeholder="Email" type="email" tabindex="8" />
                     </CInputGroup>
                     <CInputGroup class="mb-4">
                       <CInputGroupText>
                         <CIcon icon="cil-lock-unlocked" />
                       </CInputGroupText>
-                      <CFormInput type="password" placeholder="Confirmar contraseña" tabindex="10"/>
+                      <CFormInput v-model="formData.password_confirmed" type="password" placeholder="Confirmar contraseña" tabindex="10" />
                     </CInputGroup>
                   </div>
                 </div>
                 <div class="d-grid">
-                  <CButton color="success">Registrate!</CButton>
+                  <CButton  @click="registerUser" color="success">Registrate!</CButton>
                 </div>
               </CForm>
             </CCardBody>
@@ -89,132 +113,108 @@
 </template>
 
 <script>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+import { ref, onBeforeMount } from 'vue';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+
 export default {
   name: 'Register',
-}
+  setup() {
+    const formData = ref({
+      type_document: '',
+      document: '',
+      first_name: '',
+      last_name: '',
+      sex: '',
+      address: '',
+      phone: '',
+      email: '',
+      password: '',
+      password_confirmed: '',
+    });
 
-const showPassword = ref(false);
+    const errors = ref({
+      type_document: [],
+      document: [],
+      first_name: [],
+      last_name: [],
+      sex: [],
+      address: [],
+      phone: [],
+      email: [],
+      password: [],
+      password_confirmed: [],
+    });
 
-const formData = ref({
-    type_document: '',
-    document: '',
-    first_name: '',
-    last_name: '',
-    sex: '',
-    phone: '',
-    birthdate: '',
-    address: '',
-    city: '',
-    state: '',
-    neighborhood: '',
-    email: '',
-    password: '',
-    password_confirmed: '',
-    affilliate_type: '',
-    eps_id: '',
-    termsAccepted: '',
-});
+    const epsList = ref([]);
 
-const errors = ref({
-    type_document: [],
-    document: [],
-    first_name: [],
-    last_name: [],
-    sex: [],
-    phone: [],
-    birthdate: [],
-    address: [],
-    city: [],
-    state: [],
-    neighborhood: [],
-    email: [],
-    password: [],
-    password_confirmed: [],
-    affilliate_type: [],
-    eps_id: [],
-    termsAccepted: '',
-});
-
-const epsList = ref([]);
-
-const errorsClear = () => {
-    errors.value = {
+    const errorsClear = () => {
+      errors.value = {
         type_document: [],
         document: [],
         first_name: [],
         last_name: [],
         sex: [],
-        phone: [],
-        birthdate: [],
         address: [],
-        city: [],
-        state: [],
-        neighborhood: [],
+        phone: [],
         email: [],
         password: [],
         password_confirmed: [],
-        affilliate_type: [],
-        eps_id: [],
-        termsAccepted: '',
+      }
     }
+
+    
+  const registerUser = async () => {
+      errorsClear()
+
+      let has_error = false;
+      Object.entries(formData.value).forEach(f => {
+          const elemento = f[0]
+          const value = f[1]
+          if (value == "") {
+              has_error = true
+              errors.value[elemento] = "Este campo es obligatorio"
+          }
+      });
+
+      if (has_error) return;
+
+      try {
+          const response = await axios.post('http://inventario-backend.test/api/register', formData.value);
+
+          Swal.fire({
+              title: 'Éxito!',
+              text: 'Usuario registrado correctamente!',
+              icon: 'success',
+              confirmButtonText: '¡Entendido!'
+          });
+
+          router.push({ name: 'login' })
+
+      } catch (error) {
+          if (error.response && error.response.data && error.response.data.errors) {
+              const errors_api = error.response.data.errors;
+              Object.entries(errors_api).forEach(e => {
+                  const elemento = e[0]
+                  const mensaje = e[1]
+                  errors.value[elemento] = mensaje
+              });
+          } else {
+              alert("server error")
+          }
+      }
+  };
+
+    return {
+      formData,
+      errors,
+      epsList,
+      errorsClear,
+      registerUser
+    };
+  }
 }
-
-const openTermsModal = () => {
-    const modal = new bootstrap.Modal(document.getElementById('termsModal'), {
-        keyboard: false,
-        backdrop: 'static'
-    });
-    modal.show();
-};
-
-const registerUser = async () => {
-    errorsClear()
-
-    let has_error = false;
-    Object.entries(formData.value).forEach(f => {
-        const elemento = f[0]
-        const value = f[1]
-        if (value == "") {
-            has_error = true
-            errors.value[elemento] = "Este campo es obligatorio"
-        }
-    });
-
-    if (has_error) return;
-
-    try {
-        const response = await axios.post('http://consultorio.test/api/register', formData.value);
-
-        Swal.fire({
-            title: 'Éxito!',
-            text: 'Usuario registrado correctamente!',
-            icon: 'success',
-            confirmButtonText: '¡Entendido!'
-        });
-
-        router.push({ name: 'login' })
-
-    } catch (error) {
-        if (error.response && error.response.data && error.response.data.errors) {
-            const errors_api = error.response.data.errors;
-            Object.entries(errors_api).forEach(e => {
-                const elemento = e[0]
-                const mensaje = e[1]
-                errors.value[elemento] = mensaje
-            });
-        } else {
-            alert("server error")
-        }
-    }
-};
-
-const showEPS = async () => {
-    try {
-        const response = await axios.get('http://consultorio.test/api/epsPublic');
-        epsList.value = response.data.data;
-    } catch (error) {
-        console.error('Error al obtener las EPS', error);
-    }
-};
 </script>
-
